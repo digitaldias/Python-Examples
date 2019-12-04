c87b535e980f34107093a54706be54a696ae3905
Pedro G. Dias <pedro@digitaldias.com>
2019-12-04 12:30:18 +0100

    Remove random angle function and other unused stuff

--- code/TreeKata_NonOop.py
+++ code/TreeKata_NonOop.py
@@ -1,6 +1,7 @@
 # Code Kata: "the tree"
 # Original concept by Seb-Lee Delisle  @seblee
 # Adapted to Python by Pedro Dias @digitaldias December 4, 2019
+# This is the non-OOP version of this tree.
 # Revision 1.0
 
 from tkinter import *
@@ -8,13 +9,14 @@ import math
 import random
 import time
 
+instructions = "Click the left mousebutton to draw a tree.\nKeep clicking until bored sick.\nHit <ESC> to end the misery"
+
 # Initial junk, set up a window to put the tree in
 window = Tk()
 window.title("A Tree")
 window.configure(background='black')
 window.geometry("1024x768")
 
-instructions = "Click the left mousebutton to draw a tree.\nRepeat until bored sick.\nHit <ESC> to end the misery"
 
 # Create a canvas to draw on
 canvas = Canvas(window, bd=0, highlightthickness=0, relief='ridge')
@@ -22,15 +24,12 @@ canvas.configure(background='black', bd=0)
 canvas.create_text(10, 10, text=instructions, fill='white', font="arial 12", anchor=NW)
 canvas.pack(fill=BOTH, expand=True)
 
-# Get our Canvas dimensions
+
+# Get our Canvas dimensions and print them in the console
 window.update()
 width, height = canvas.winfo_width(), canvas.winfo_height()
 print("Window width: {}, height: {}".format(width, height))
 
-def randomDegree(min, max):
-    """ Return a random degree between min and max """
-
-    return min + random.randint(min, max)
 
 def reduce_branch_length(size):
     """ Reduce the branch by a random length of 60-95% """
@@ -49,23 +48,29 @@ def drawTree(size, lastX, lastY, angle):
 
     canvas.create_line(lastX, lastY, nextX, nextY, fill='yellow', width=lineWidth)
 
+    # Enter the recursion
     if size > 15:
         branchLength = reduce_branch_length(size)
-        drawTree(branchLength, nextX, nextY, angle + randomDegree(0, 35))
+        drawTree(branchLength, nextX, nextY, angle + random.uniform(0, 35))
 
         branchLength = reduce_branch_length(size)
-        drawTree(branchLength, nextX, nextY, angle - randomDegree(0, 35))
+        drawTree(branchLength, nextX, nextY, angle - random.uniform(0, 35))
 
-def start_over(_):
+
+def display_tree(_):
     """ Empties the current canvas and draws a new tree """
 
     canvas.delete("all")
-    t0 = time.time()
     lastX, lastY = canvas.winfo_width() / 2, canvas.winfo_height() - 10  # Calculate the starting positions bottom center
     treeSize = canvas.winfo_height() * (1/7.68)                          # Height of the tree is in the ratio 768pix height goes to 100pix tall tree
+    t0 = time.time()                                                     # time it
+
+    # Enough already! Let's draw it!
     drawTree(treeSize, lastX, lastY, 0)
+
     finished = "{}ms.".format(round(1000* (time.time() - t0), 1))
     canvas.create_text(lastX + 12, lastY -12, text=finished, fill='green', anchor=NW ) # informal "grass" with time taken to render :D
+
 
 def end_program(_):
     """ Terminate the program """
@@ -73,23 +78,26 @@ def end_program(_):
     print("Program ended.")
     window.destroy()
 
-def configure(event):
+
+def redraw_window(event):
     """ When the window resizes, make sure to update """
 
     window.update()
     canvas.update()
     canvas.delete("all")
+
+    # Print the new size to console
     width, height = canvas.winfo_width(), canvas.winfo_height()
     print("Resize event to: {}x{}".format(width, height))
 
-    # Find our starting position at the middle of the bottom of the screen
+    # Repeat our instructions after clearing the screen
     lastX = width/2
     lastY = height - 10
     canvas.create_text(10, 10, text=instructions, fill='white', font="arial 12", anchor=NW)
 
 
 if __name__ == '__main__':
-    canvas.bind("<Button-1>", start_over) # Bind left button to redrawing the tree
-    window.bind("<Escape>", end_program)  # Let the program finish when ESC is pressed
-    canvas.bind("<Configure>", configure) # What to do when the window resizes
+    canvas.bind("<Button-1>", display_tree)     # Bind left button to redrawing the tree
+    window.bind("<Escape>", end_program)      # Let the program finish when ESC is pressed
+    canvas.bind("<Configure>", redraw_window) # What to do when the window resizes
     window.mainloop()
